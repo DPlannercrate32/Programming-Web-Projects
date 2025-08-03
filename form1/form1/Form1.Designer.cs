ce5b5dbd2c1aaccac454d09035606f8a4cfe946a
DPlannercrate32 <dddavis717@gmail.com>
2025-08-03 16:29:23 -0400

    Add files via upload

---
+++ form1/form1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extractAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(410, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.addFilesToolStripMenuItem,
+            this.extractAllToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // addFilesToolStripMenuItem
+            // 
+            this.addFilesToolStripMenuItem.Name = "addFilesToolStripMenuItem";
+            this.addFilesToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.addFilesToolStripMenuItem.Text = "Add Files";
+            // 
+            // extractAllToolStripMenuItem
+            // 
+            this.extractAllToolStripMenuItem.Name = "extractAllToolStripMenuItem";
+            this.extractAllToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.extractAllToolStripMenuItem.Text = "Extract All";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(0, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(410, 297);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 331);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extractAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ form1/form1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*Note to viewers: This function below handles whatever text is dragged into the form by checking if the text is part of
+         an URL before allowing it to be dragged into the form. Also test this program out by dragging any text from the internet browser and dropping it into this programs windows form.*/
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+            string hold = null;
+            hold += e.Data.GetData(DataFormats.Text).ToString(); //any data that gets dropped into the windows form is added to the hold string. 
+            string temp = "";
+            bool urlmode = false;
+
+            for (int k = hold.Length - 1; k >= 0; k--) //looping through hold string in reverse to checking for top level domain such as ".com", "edu", or ".net" to find the end of a URL.
+            {
+                if ((hold[k] == 'm' && hold[k - 1] == 'o' && hold[k - 2] == 'c' && hold[k - 3] == '.') || (hold[k] == 'u' && hold[k - 1] == 'd' && hold[k - 2] == 'e' && hold[k - 3] == '.') || (hold[k] == 'v' && hold[k - 1] == 'o' && hold[k - 2] == 'g' && hold[k - 3] == '.') || (hold[k] == 't' && hold[k - 1] == 'e' && hold[k - 2] == 'n' && hold[k - 3] == '.'))
+                {
+                    urlmode = true; 
+                }
+                if (urlmode == false)
+                    continue;
+
+                if(urlmode == true)  // if true then start add characters to the temporary string. 
+                {
+                    temp += hold[k];
+                    Console.WriteLine(temp); // testing to see if something is in temp. 
+                }
+                if(k <= 0 || hold[k] == ' ' || hold[k] == '\n') //if a space, a newline, or beginning of hold string is reached, then stop add characters to temp and transfer characters from temp to the textBox1.Text string a.k.a. the windows form. 
+                {
+                    urlmode = false;
+                    for(int d = temp.Length - 1; d >= 0; d--)
+                    {
+                        textBox1.Text += temp[d];
+                    }
+                    textBox1.Text += ' '; // plus add a space after each URL statement
+                    temp = null;          // emptying the temp variable. 
+                }
+            }
+            hold = null; // clearing out the hold string to prepare for the next possible url string to fill it up. 
+        }
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) //This function starts the process of browsing which .txt file to open once the "Open" option is clicked in the file dropdown menu.
+        {
+            Stream str;
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Texts Only | *.txt| All Files|*.*";
+            fd.ShowDialog();
+            if((str = fd.OpenFile()) != null)
+            {
+                string fn = fd.FileName;
+                string filetext = File.ReadAllText(fn);
+                textBox1.Text = filetext;
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e) // This function begins the "Quit" button is clicked in the file dropdown menu & it basically closes the application.
+        {
+            Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)// This function starts after the "New" option is clicked in the file dropdown menu and it erases any text from the form. 
+        {
+            textBox1.Text = null;
+        }
+    }
+}
